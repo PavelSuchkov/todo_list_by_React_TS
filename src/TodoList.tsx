4e519e923a4100c92442e3f60c78e85e48ab084b
PavelSuchkov <suchkovp@gmail.com>
2021-04-21 16:11:47 +0300

    9. Optimization to rendering was done

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -6,7 +6,7 @@ import {Button, IconButton} from "@material-ui/core";
 import {Delete} from "@material-ui/icons";
 import {useDispatch, useSelector} from "react-redux";
 import {AppRootStateType} from "./state/store";
-import {addTaskAC, changeTaskStatusAC, changeTitleAC, removeTaskAC} from "./state/tasks-reducer";
+import {addTaskAC} from "./state/tasks-reducer";
 import {Task} from "./components/Task";
 
 
@@ -67,22 +67,24 @@ export const TodoList = React.memo(({
 
     const filteredTasks = tasksForTodoList.map((t) => {
 
-        const removeTask = (id: string) => dispatch(removeTaskAC(id, todoListID));
 
-        const changeTaskStatus = (taskId: string, isDone: boolean) =>
-            dispatch(changeTaskStatusAC(taskId, isDone, todoListID))
 
-        const changeTaskTitle = (taskId: string, newTitle: string) => {
-            dispatch(changeTitleAC(taskId, newTitle, todoListID))
-        }
+        // const changeTaskStatus = (taskId: string, isDone: boolean) =>
+        //     dispatch(changeTaskStatusAC(taskId, isDone, todoListID))
+        //
+        // const changeTaskTitle = (taskId: string, newTitle: string) => {
+        //     dispatch(changeTitleAC(taskId, newTitle, todoListID))
+        // }
 
         return (
-            <Task task={t}
+            <Task
+                todolistId={todoListID}
+                task={t}
                   isDone={t.isDone}
                   title={t.title}
-                  changeTaskStatus={changeTaskStatus}
-                  changeTaskTitle={changeTaskTitle}
-                  removeTask={removeTask}
+                  // changeTaskStatus={changeTaskStatus}
+                  // changeTaskTitle={changeTaskTitle}
+                  // removeTask={removeTask}
                   key={t.id}/>
         )
     })
--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -3,39 +3,55 @@ import {EditableSpan} from "./EditableSpan";
 import {Delete} from "@material-ui/icons";
 import React, {ChangeEvent, useCallback} from "react";
 import {TaskType} from "../AppWithRedux";
+import {changeTaskStatusAC, changeTitleAC, removeTaskAC} from "../state/tasks-reducer";
+import {useDispatch} from "react-redux";
 
 
 type taskPropsType = {
+    todolistId: string
     task: TaskType
     isDone: boolean
     title: string
-    changeTaskStatus: (id: string, isDone: boolean) => void
-    changeTaskTitle: (id: string, title: string) => void
-    removeTask: (taskId: string) => void
+    // changeTaskStatus: (id: string, isDone: boolean) => void
+    // changeTaskTitle: (id: string, title: string) => void
+    // removeTask: (taskId: string) => void
 }
 
 export const Task = React.memo(({
+                                    todolistId,
                                     task,
                                     isDone,
                                     title,
-                                    changeTaskStatus,
-                                    changeTaskTitle,
-                                    removeTask
+                                    // changeTaskStatus,
+                                    // changeTaskTitle,
+                                    // removeTask
                                 }: taskPropsType) => {
 
     console.log('Task was rendered')
+    const dispatch = useDispatch()
+
+
+
+    const changeTaskTitle = useCallback((taskId: string, newTitle: string) => {
+        dispatch(changeTitleAC(taskId, newTitle, todolistId))}, [dispatch, todolistId]);
 
     const changeTaskTitleCB = useCallback((newTitle: string) => {
         changeTaskTitle(task.id, newTitle)
     }, [changeTaskTitle, task.id])
 
+
+    const changeTaskStatus = useCallback((taskId: string, isDone: boolean) =>
+        dispatch(changeTaskStatusAC(taskId, isDone, todolistId)), [dispatch, todolistId])
+
+
     const changeTaskStatusCB = useCallback((e: ChangeEvent<HTMLInputElement>) => {
         changeTaskStatus(task.id, e.currentTarget.checked)
     }, [changeTaskStatus, task.id])
 
-    const removeTaskCB = useCallback(() => {
-        removeTask(task.id)
-    }, [removeTask, task.id])
+
+    const removeTask = useCallback(() => {
+        dispatch(removeTaskAC(task.id, todolistId))
+    }, [dispatch, task.id, todolistId])
 
     return (
         <li className={isDone ? "is-done" : ''}>
@@ -43,7 +59,7 @@ export const Task = React.memo(({
                       color={'primary'}
                       onChange={changeTaskStatusCB}/>
             <EditableSpan title={title} changeTitle={changeTaskTitleCB}/>
-            <IconButton aria-label="delete" onClick={removeTaskCB} color={'primary'}>
+            <IconButton aria-label="delete" onClick={removeTask} color={'primary'}>
                 <Delete/>
             </IconButton>
         </li>
